8b6a1c7319ac3cb113d4ec52d62059615ee1919f
Juan Arias <jcamiloat@gmail.com>
2022-09-13 18:27:26 -0500

    Done - Ex1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,7 +6,38 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  // const incrementGood = () => {
+  //   setGood(good + 1);
+  // };
+
+  // const incrementNeutral = () => {
+  //   setNeutral(neutral + 1);
+  // };
+
+  // const incrementBad = () => {
+  //   setBad(bad + 1);
+  // };
+
+  const incrementState = (setState, state) => () => {
+    setState(state + 1);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Give Feedback</h1>
+        <button onClick={incrementState(setGood, good)}>Good</button>
+        <button onClick={incrementState(setNeutral, neutral)}>Neutral</button>
+        <button onClick={incrementState(setBad, bad)}>Bad</button>
+      </div>
+      <div>
+        <h1>Statistics</h1>
+        <div>Good {good}</div>
+        <div>Neutral {neutral}</div>
+        <div>Bad {bad}</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
