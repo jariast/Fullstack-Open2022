bcf8c9190e1e73fb707c655d70a0fc32702eaea4
Juan Arias <jcamiloat@gmail.com>
2022-09-14 10:15:16 -0500

    Ex 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,7 +13,20 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  const getRandomIndex = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const nextAnecdote = () => {
+    setSelected(getRandomIndex(anecdotes.length));
+  };
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={nextAnecdote}>Next Anecdote</button>
+    </div>
+  );
 };
 
 export default App;
