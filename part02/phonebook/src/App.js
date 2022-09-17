5594868150131b9cacc25355fce4178c7b6ed9e0
Juan Arias <jcamiloat@gmail.com>
2022-09-17 08:24:30 -0500

    Ex 2.15 Phonebook step 7

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -37,9 +37,11 @@ const App = () => {
       return;
     }
     const newPerson = { name: newName, number: newPhoneNumber };
-    setPersons(persons.concat(newPerson));
-    setNewName('');
-    setNewPhoneNumber('');
+    axios.post('http://localhost:3001/persons', newPerson).then((res) => {
+      setPersons(persons.concat(res.data));
+      setNewName('');
+      setNewPhoneNumber('');
+    });
   };
 
   return (
