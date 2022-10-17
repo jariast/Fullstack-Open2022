09739dd0a565a0714c014a39773524ca7e4da57e
Juan Arias <jcamiloat@gmail.com>
2022-10-17 10:19:58 -0500

    feat: Exercise 5.7
    Added Blog details visibility functionality.

--- part05/bloglist-frontend/src/components/Blog.js
+++ part05/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import styled from 'styled-components';
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetailsViz = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <Wrapper>
+      {blog.title} {blog.author}{' '}
+      <button onClick={toggleDetailsViz}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {showDetails && (
+        <DetailsWrapper>
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </DetailsWrapper>
+      )}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  border: 1px solid gray;
+  margin-bottom: 10px;
+`;
+
+const DetailsWrapper = styled.div``;
+
+export default Blog;
