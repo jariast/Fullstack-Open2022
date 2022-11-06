958eab57e649cf1135e2a3f293163daa538adefe
Camilo Arias <jcamiloat@gmail.com>
2022-11-05 20:51:57 -0500

    chore: Moved blogs list into own component.

--- part05/bloglist-frontend/src/App.js
+++ part05/bloglist-frontend/src/App.js
@@ -1,28 +1,19 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Login from './components/Login';
 import blogService from './services/blogs';
 import loginService from './services/login';
-import Togglable from './components/Togglable';
-import Blog from './components/blog/Blog';
-import BlogForm from './components/blogsForm/BlogsForm';
 import Notification from './components/notification/Notification';
 import { showNotification } from './components/notification/notificationSlice';
+import BlogsList from './components/blog/BlogsList';
 
 const App = () => {
   const dispatch = useDispatch();
-  const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const [user, setUser] = useState(null);
-  const blogFormRef = useRef();
-  const sortByLikes = (blog1, blog2) => blog2.likes - blog1.likes;
-
-  useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs.sort(sortByLikes)));
-  }, []);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('user');
@@ -53,73 +44,6 @@ const App = () => {
     setUser(null);
   };
 
-  const handleBlogCreation = async (newBlog) => {
-    try {
-      blogFormRef.current.toggleVisibility();
-      const createdBlog = await blogService.createBlog(user.token, newBlog);
-
-      setBlogs(blogs.concat(createdBlog));
-
-      dispatch(
-        showNotification(
-          `A new blog "${createdBlog.title}" by ${createdBlog.author} added`
-        )
-      );
-    } catch (error) {
-      console.log('Blog creation error', error);
-      dispatch(showNotification(error.response.data.error, true));
-    }
-  };
-
-  const handleBlogLike = async (blogToLike) => {
-    try {
-      const updatedBlog = await blogService.updateLikes(
-        blogToLike.id,
-        blogToLike
-      );
-      setBlogs(
-        blogs
-          .map((blog) => (blog.id === updatedBlog.id ? updatedBlog : blog))
-          .sort(sortByLikes)
-      );
-    } catch (error) {
-      console.log('Blog liking error', error);
-    }
-  };
-
-  const handleBlogDeletion = async (blogId) => {
-    try {
-      await blogService.deleteBlog(blogId);
-      setBlogs(blogs.filter((blog) => blog.id !== blogId));
-    } catch (error) {
-      console.log('Error Deleting blog', error);
-      dispatch(showNotification(error.response.data.error, true));
-    }
-  };
-
-  const blogsList = () => (
-    <>
-      <h2>blogs</h2>
-      <p>{`${user.name} is logged in`}</p>
-      <button id="logout-button" onClick={handleLogout}>
-        Log out
-      </button>
-      <Togglable buttonLabel="New Blog" ref={blogFormRef}>
-        <BlogForm addBloghandler={handleBlogCreation}></BlogForm>
-      </Togglable>
-
-      {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          updateBlogHandler={handleBlogLike}
-          user={user}
-          deleteBlogHandler={handleBlogDeletion}
-        />
-      ))}
-    </>
-  );
-
   return (
     <div>
       {user === null ? (
@@ -131,7 +55,14 @@ const App = () => {
           newPasswordHandler={({ target }) => setPassword(target.value)}
         ></Login>
       ) : (
-        blogsList()
+        <>
+          <h2>blogs</h2>
+          <p>{`${user.name} is logged in`}</p>
+          <button id="logout-button" onClick={handleLogout}>
+            Log out
+          </button>
+          <BlogsList user={user}></BlogsList>
+        </>
       )}
 
       <Notification />
---
+++ part05/bloglist-frontend/src/components/blog/BlogsList.js
@@ -0,0 +1,84 @@
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+
+import blogService from '../../services/blogs';
+import Togglable from '../Togglable';
+import Blog from './Blog';
+import BlogForm from '../blogsForm/BlogsForm';
+import { showNotification } from '../notification/notificationSlice';
+
+const BlogsList = ({ user }) => {
+  const dispatch = useDispatch();
+  const [blogs, setBlogs] = useState([]);
+  const blogFormRef = useRef();
+  const sortByLikes = (blog1, blog2) => blog2.likes - blog1.likes;
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs.sort(sortByLikes)));
+  }, []);
+
+  const handleBlogCreation = async (newBlog) => {
+    try {
+      blogFormRef.current.toggleVisibility();
+      const createdBlog = await blogService.createBlog(user.token, newBlog);
+
+      setBlogs(blogs.concat(createdBlog));
+
+      dispatch(
+        showNotification(
+          `A new blog "${createdBlog.title}" by ${createdBlog.author} added`
+        )
+      );
+    } catch (error) {
+      console.log('Blog creation error', error);
+      dispatch(showNotification(error.response.data.error, true));
+    }
+  };
+
+  const handleBlogLike = async (blogToLike) => {
+    try {
+      const updatedBlog = await blogService.updateLikes(
+        blogToLike.id,
+        blogToLike
+      );
+      setBlogs(
+        blogs
+          .map((blog) => (blog.id === updatedBlog.id ? updatedBlog : blog))
+          .sort(sortByLikes)
+      );
+    } catch (error) {
+      console.log('Blog liking error', error);
+    }
+  };
+
+  const handleBlogDeletion = async (blogId) => {
+    try {
+      await blogService.deleteBlog(blogId);
+      setBlogs(blogs.filter((blog) => blog.id !== blogId));
+      dispatch(showNotification('Deleted blog'));
+    } catch (error) {
+      console.log('Error Deleting blog', error);
+      dispatch(showNotification(error.response.data.error, true));
+    }
+  };
+
+  return (
+    <>
+      <Togglable buttonLabel="New Blog" ref={blogFormRef}>
+        <BlogForm addBloghandler={handleBlogCreation}></BlogForm>
+      </Togglable>
+
+      {blogs.map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          updateBlogHandler={handleBlogLike}
+          user={user}
+          deleteBlogHandler={handleBlogDeletion}
+        />
+      ))}
+    </>
+  );
+};
+
+export default BlogsList;
