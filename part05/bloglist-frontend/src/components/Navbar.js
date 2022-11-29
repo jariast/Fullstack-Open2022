c7a7d2cadd3b7c3ee02f191dfbd0465e2c80b07a
Camilo Arias <jarias+ARAY@accuray.com>
2022-11-29 15:48:12 -0500

    feat: Bootstrap Navbar.

--- part05/bloglist-frontend/src/components/Navbar.js
+++ part05/bloglist-frontend/src/components/Navbar.js
@@ -1,6 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { selectLoggedUser, userLoggedOut } from './user/usersSlice';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from 'react-bootstrap';
 
 const NavBar = () => {
   const user = useSelector(selectLoggedUser);
@@ -12,20 +15,27 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-      <span>{`${user.name} is Logged In`}</span>
-      <button id="logout-button" onClick={handleLogout}>
-        Log out
-      </button>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" as="span">
+              <Link to="/blogs">Blogs</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link to="/users">Users</Link>
+            </Nav.Link>
+            <Navbar.Text>
+              Signed in as: {user.name}{' '}
+              <button id="logout-button" onClick={handleLogout}>
+                Log out
+              </button>
+            </Navbar.Text>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
