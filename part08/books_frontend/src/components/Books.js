f9794a8e5f7501ebaad3c5a49ff7af9707279db6
Camilo Arias <jarias+ARAY@accuray.com>
2022-12-16 09:03:23 -0500

    feat: Ex 8.9 Books view.
    Added allBooks query.
    Called query on Books component.

--- part08/books_frontend/src/components/Books.js
+++ part08/books_frontend/src/components/Books.js
@@ -1,9 +1,16 @@
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
+
 const Books = (props) => {
+  const { data, loading } = useQuery(ALL_BOOKS);
+
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  if (loading) {
+    return <div>loading</div>;
+  }
 
   return (
     <div>
@@ -16,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
@@ -26,7 +33,7 @@ const Books = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
--- part08/books_frontend/src/queries.js
+++ part08/books_frontend/src/queries.js
@@ -10,3 +10,14 @@ export const ALL_AUTHORS = gql`
     }
   }
 `;
+
+export const ALL_BOOKS = gql`
+  query AllBooks {
+    allBooks {
+      author
+      id
+      published
+      title
+    }
+  }
+`;
