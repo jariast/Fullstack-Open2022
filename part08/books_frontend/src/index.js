f173544076b2fad89e42f8d47ab3a367c9b2696c
Camilo Arias <jcamiloat@gmail.com>
2023-02-28 09:15:30 -0500

    feat: 8.18 Login
    Added auth header to requests.

--- part08/books_frontend/src/index.js
+++ part08/books_frontend/src/index.js
@@ -5,15 +5,28 @@ import App from './App';
 import {
   ApolloClient,
   ApolloProvider,
-  HttpLink,
+  createHttpLink,
   InMemoryCache,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers }) => {
+  const token = JSON.parse(localStorage.getItem('books-app-user-token'));
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  }),
+  link: authLink.concat(httpLink),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
