c65618331ab161bb91635e1e89e364fe51a936ba
Camilo Arias <jcamiloat@gmail.com>
2023-03-03 08:35:31 -0500

    chore: Removing empty string in index.js

--- part08/books_frontend/src/index.js
+++ part08/books_frontend/src/index.js
@@ -31,7 +31,6 @@ const client = new ApolloClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
-    {' '}
     <App />
   </ApolloProvider>
 );
