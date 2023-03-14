85770bbced93c6c8ae335b89b282029b24855274
Camilo Arias <jcamiloat@gmail.com>
2023-03-14 14:36:53 -0500

    feat: 9.3 Command line - Calculate Exercises.
    Modified Exercise Calc to work from the command line.

--- part09/nodeApp/exerciseCalculator.ts
+++ part09/nodeApp/exerciseCalculator.ts
@@ -1,5 +1,45 @@
 import { ExerciseInfo } from './interfaces/ExceriseInfo';
-import { getAverage } from './utils';
+import { getAverage, isNotNumber } from './utils';
+
+try {
+  const { target, trainingHoursByDay } = parseConsoleArgs(process.argv);
+  console.log(
+    'Exercise Info: ',
+    calculateExcercises(target, trainingHoursByDay)
+  );
+} catch (error: any) {
+  let errMsg = 'Something went wrong.';
+  if (error instanceof Error) {
+    errMsg += ' Error: ' + error.message;
+  }
+
+  console.log(errMsg);
+}
+
+function parseConsoleArgs([
+  _,
+  __,
+  target,
+  ...trainingHoursByDay
+]: string[]): ExerciseValues {
+  if (!target || !trainingHoursByDay) {
+    throw new Error('Please provide at least two arguments');
+  }
+
+  if (
+    isNotNumber(target) ||
+    trainingHoursByDay.some((hour) => isNotNumber(hour))
+  ) {
+    throw new Error('Please provide only numbers');
+  }
+
+  return {
+    target: Number(target),
+    trainingHoursByDay: trainingHoursByDay.map((hourString) =>
+      Number(hourString)
+    ),
+  };
+}
 
 function calculateExcercises(
   target: number,
@@ -25,4 +65,8 @@ function hasTrainedThatDay(day: number) {
   return day !== 0;
 }
 
-console.log('Calculate Ex: ', calculateExcercises(2, [3, 0, 2, 4.5, 0, 3, 1]));
+// console.log('Calculate Ex: ', calculateExcercises(2, [3, 0, 2, 4.5, 0, 3, 1]));
+interface ExerciseValues {
+  target: number;
+  trainingHoursByDay: number[];
+}
