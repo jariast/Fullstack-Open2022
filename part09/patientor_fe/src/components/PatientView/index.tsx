6057ed650170cfa8c275d65fd1266e60e4a7e7c1
Camilo Arias <jcamiloat@gmail.com>
2023-03-30 10:58:40 -0500

    feat: 9.21: Patientor, step2 - Single Patient View
    Added PatientView component.
    
    Added getPatientById method to patient service.

--- part09/patientor_fe/src/components/PatientView/index.tsx
+++ part09/patientor_fe/src/components/PatientView/index.tsx
@@ -1,5 +1,68 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Gender, Patient } from '../../types';
+import patientService from '../../services/patients';
+import axios from 'axios';
+import { Female, Male, Transgender } from '@mui/icons-material';
+
 const PatientView = () => {
-  return <p>Hello Patient</p>;
+  const [patient, setPatient] = useState<Patient | null>(null);
+  const [error, setError] = useState('');
+
+  const patientId = useParams().id;
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      if (!patientId) {
+        setError('Patiend ID is missing');
+        return;
+      }
+
+      try {
+        const patient = await patientService.getPatientById(patientId);
+        setPatient(patient);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.response?.data);
+          console.log('Error', error.message);
+        }
+      }
+    };
+
+    void fetchPatient();
+  }, []);
+
+  let icon;
+
+  switch (patient?.gender) {
+    case Gender.Male:
+      icon = <Male></Male>;
+      break;
+    case Gender.Female:
+      icon = <Female></Female>;
+      break;
+    case Gender.Other:
+      icon = <Transgender></Transgender>;
+      break;
+    default:
+      break;
+  }
+
+  const content = (
+    <>
+      <h2>
+        {patient?.name} {icon}
+      </h2>
+      <p>SSN: {patient?.ssn}</p>
+      <p>Ocuupation: {patient?.occupation}</p>
+    </>
+  );
+  return (
+    <>
+      {error && <h3>{error}</h3>}
+      {patient && content}
+    </>
+  );
 };
 
 export { PatientView };
